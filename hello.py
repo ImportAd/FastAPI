f329d06a27178481f706026389a7dd7b4d57a810
ImportAd <musin.f.mail@gmail.com>
2025-08-13 14:49:37 +0300

    Introduce POST request parameters

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
-@app.get("/hi/")
-def greet(who: str):
+@app.post("/hi")
+def greet(who: str = Header()):
     return {f"Hello? {who}?"}
 
 if __name__ == "__main__":
